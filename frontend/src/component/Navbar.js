e7f405e3b63196393594e236a82dbe179b91451d
spyer <sundayomoladee11@gmail.com>
2025-03-01 00:56:37 +0100

    adding category

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,33 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/navbar.css'
 import { GiHamburgerMenu } from "react-icons/gi";
 import Nav from 'react-bootstrap/Nav';
+import { FiPhone } from "react-icons/fi";
 const Navbarr = () => {
+  const [moveOut,setMoveout]=useState(false)
   return (
     <nav className='navbarContainer'>
       <aside className='navbarleftside'><GiHamburgerMenu /><span>All Categories</span></aside>
       <section>
-      <Nav
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
+        <article className='outerCategoryContainer'>
+              <Nav>
+            <Nav.Item>
+              <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link >Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link to=''>Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link >Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>Link</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </article>
+      <Nav>
       <Nav.Item>
         <Nav.Link href="/home">Active</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
+        <Nav.Link>Link</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
+        <Nav.Link >Link</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
+        <Nav.Link to=''>Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link >Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link>Link</Nav.Link>
       </Nav.Item>
     </Nav>
       </section>
-      <aside></aside>
+      <aside>
+      <FiPhone /><span><b>Call us:</b>+2347069258526</span>
+      </aside>
   </nav>
   );
 }
