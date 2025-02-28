38b18d67fd7ef256d357f0762804711227d0dd01
spyer <sundayomoladee11@gmail.com>
2025-02-28 23:56:28 +0100

    adding category

--- frontend/src/component/Category.js
+++ frontend/src/component/Category.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Accordion from 'react-bootstrap/Accordion';
-import { FaPlus, FaMinus } from 'react-icons/fa';
 import "../styles/pageNavbar.css"
 import Nav from 'react-bootstrap/Nav';
-import { MdOutlineShoppingCart } from "react-icons/md";
-import Badge from 'react-bootstrap/Badge';
-const Category = () => {
-    const [activeKey, setActiveKey] = useState(null);
 
-    const handleToggle = (key) => {
-      setActiveKey(activeKey === key ? null : key);
-    };
+const Category = () => {
   
     return (
       <Accordion defaultActiveKey="0">
           <Accordion.Item>
         <Accordion.Header className='blognavsmall'>
-          <Nav.Link className='blognavsmall'>Blogs</Nav.Link>
+          <Nav.Link className='blognavsmall'>Electronics</Nav.Link>
         </Accordion.Header>
       </Accordion.Item>
           <Accordion.Item>
         <Accordion.Header className='blognavsmall'>
-          <Nav.Link className='blognavsmall'>Blogs</Nav.Link>
+          <Nav.Link className='blognavsmall'>Health Products</Nav.Link>
         </Accordion.Header>
       </Accordion.Item>
         <Accordion.Item>
         <Accordion.Header className='blognavsmall'>
-          <Nav.Link className='blognavsmall'>Blogs</Nav.Link>
+          <Nav.Link className='blognavsmall'>Fashion</Nav.Link>
         </Accordion.Header>
       </Accordion.Item>
       <Accordion.Item>
         <Accordion.Header className='blognavsmall'>
-          <Nav.Link className='blognavsmall'>Blogs</Nav.Link>
+          <Nav.Link className='blognavsmall'>Auto Mobiles</Nav.Link>
         </Accordion.Header>
       </Accordion.Item>
-     
+      <Accordion.Item>
+        <Accordion.Header className='blognavsmall'>
+          <Nav.Link className='blognavsmall'>Phones</Nav.Link>
+        </Accordion.Header>
+      </Accordion.Item>
+          <Accordion.Item>
+        <Accordion.Header className='blognavsmall'>
+          <Nav.Link className='blognavsmall'>Books</Nav.Link>
+        </Accordion.Header>
+      </Accordion.Item>
+        <Accordion.Item>
+        <Accordion.Header className='blognavsmall'>
+          <Nav.Link className='blognavsmall'>Shoes</Nav.Link>
+        </Accordion.Header>
+      </Accordion.Item>
     </Accordion>
     )
 }
