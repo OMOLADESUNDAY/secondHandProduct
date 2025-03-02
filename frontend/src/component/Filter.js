c30c1ee527585ec792531facad22c7a6b6b767a5
spyer <sundayomoladee11@gmail.com>
2025-03-02 15:41:52 +0100

    message on hover

--- frontend/src/component/Filter.js
+++ frontend/src/component/Filter.js
@@ -3,10 +3,31 @@ import { GoQuestion } from "react-icons/go";
 import '../styles/filter.css'
 const Filter = () => {
     const [hover,setHover]=useState(false)
+    const [message,setMessage]=useState()
+    const displayHover=()=>{
+        setHover(true)
+        setTimeout(()=>{
+            setHover(false)
+        },800)
+    }
+   
+    
   return (
     <div>
         <h4>Advanced Search Filter</h4>
         <form className=''>
+            <div className='filtertext'>
+                <small>product type</small>
+                <GoQuestion  onMouseOver={()=>displayHover('producttype')}/>
+                <small className={hover?'hovermessage':'rmhover'}>product type</small>
+            </div>
+            <div className='filterSelectContainer'>
+                <select className='filterSelect'>
+                    <option value=""></option>
+                    <option value="Toyota">Toyota</option>
+                </select>
+            </div>
+
             <div className='filtertext'>
                 <small>product type</small>
                 <GoQuestion />
@@ -21,18 +42,8 @@ const Filter = () => {
 
             <div className='filtertext'>
                 <small>product type</small>
-                <GoQuestion />
-            </div>
-            <div className='filterSelectContainer'>
-                <select className='filterSelect'>
-                    <option value=""></option>
-                    <option value="Toyota">Toyota</option>
-                </select>
-            </div>
-
-            <div className='filtertext'>
-                <small>product type</small>
-                <GoQuestion />
+                <GoQuestion onTouchMove={displayHover} />
+                <small className={hover?'hovermessage':'rmhover'}>product type</small>
             </div>
             <div className='filterSelectContainer'>
                 <select className='filterSelect'>
