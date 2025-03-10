6a5f73cbd5848aba8814900849f15b5224c372b6
spyer <sundayomoladee11@gmail.com>
2025-03-10 13:17:01 +0100

    trending and netlify setup

--- frontend/src/pages/TrendingCategory.js
+++ frontend/src/pages/TrendingCategory.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import { trendingCategoryData } from '../data'
+import {Link} from 'react-router-dom'
+import '../styles/trendingCategory.css'
 const TrendingCategory = () => {
   return (
-    trendingCategoryData.map((trendingitem)=>{
+    <div className='trending-category-container'>
+        { trendingCategoryData.map((trendingitem)=>{
         return(
             <div key={trendingitem.id}>
-                
+                <div className='trendcat-text-container'>
+                  <h5><Link>{trendingitem.name}</Link></h5>
+                  <p><Link>{}</Link></p>
+                  <p><Link>{}</Link></p>
+                  <p><Link>{}</Link></p>
+                  <p><Link>{}</Link></p>
+                  <h5><Link>SHOW NOW <span>&gt;&gt;&gt;</span></Link></h5>
+                </div>
+                <div className='trendcat-image-container'>
+
+                </div>
 
             </div>
         )
-    })
+    })}
+    </div>
+   
    
   )
 }
