00f2649d3b6c4d50b8437ac91d472243e13fcb69
spyer <sundayomoladee11@gmail.com>
2025-03-23 23:45:37 +0100

    singleproduct

--- frontend/src/pages/SingleProduct.js
+++ frontend/src/pages/SingleProduct.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { products } from '../data'
 import { useParams } from 'react-router-dom'
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import '../styles/singleproduct.css'
 
 const SingleProduct = () => {
   let params=useParams()
@@ -15,16 +16,15 @@ const SingleProduct = () => {
   }
   return (
     <div>
-      <div className="top">
-        <div><img src={product.image} alt={product.name} /></div>
+      <div className="top-single-product">
+        <div className='image-conatiner-single-product'><img src={product.image} alt={product.name} /></div>
         <div>
           <span>Brand:</span><small>{product.brand}</small>
           <h3>{product.name}</h3>
           <p>${product.price}</p>
           <div>
-            
-            {product.sold == null || product.sold >product.no_of_stock?<div><small>No track quantity</small></div>:<div className='sold-available'><small>sold:{product.sold}</small><small>available:{product.no_of_stock}</small></div>}
-            <ProgressBar now={(product.sold/product.no_of_stock)*100} label={`${Math.floor((product.sold/product.no_of_stock)*100)}%`}   variant="danger" animated/>
+            {product.sold == null || product.sold >product.no_of_stock?<div><small>No track quantity</small></div>:<div className='sold-available'><small>InStock:{product.no_of_stock}</small><small>sold:{product.sold}</small></div>}
+            <ProgressBar now={(product.sold/(product.no_of_stock+product.sold))*100} label={`${Math.floor((product.no_of_stock/(product.sold+product.no_of_stock))*100)}%`}   variant="success" animated/>
         </div>
         </div>
       </div>
