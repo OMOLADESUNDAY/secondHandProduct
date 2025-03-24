240604d40d9412c2351692d7a9226a08a20549c5
spyer <sundayomoladee11@gmail.com>
2025-03-24 19:20:55 +0100

    singleproduct

--- frontend/src/pages/SingleProduct.js
+++ frontend/src/pages/SingleProduct.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { products } from '../data'
 import { useParams } from 'react-router-dom'
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import '../styles/singleproduct.css'
 
 const SingleProduct = () => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const handleRadioChange = (event) => {
+    setSelectedOption(event.target.value);
+    console.log(selectedOption)
+  };
+
   let params=useParams()
   const id=params.id
-  console.log(id)
   const product=products.find((productId)=>productId.id.toString() === id)
   if(!product){
     return(
@@ -17,14 +22,64 @@ const SingleProduct = () => {
   return (
     <div>
       <div className="top-single-product">
-        <div className='image-conatiner-single-product'><img src={product.image} alt={product.name} /></div>
+        <div className='image-conatiner-single-product'><img className='single-product-image' src={product.image} alt={product.name} /></div>
         <div>
           <span>Brand:</span><small>{product.brand}</small>
           <h3>{product.name}</h3>
           <p>${product.price}</p>
           <div>
             {product.sold == null || product.sold >product.no_of_stock?<div><small>No track quantity</small></div>:<div className='sold-available'><small>InStock:{product.no_of_stock}</small><small>sold:{product.sold}</small></div>}
-            <ProgressBar now={(product.sold/(product.no_of_stock+product.sold))*100} label={`${Math.floor((product.no_of_stock/(product.sold+product.no_of_stock))*100)}%`}   variant="success" animated/>
+            <ProgressBar now={Math.floor((product.no_of_stock/(product.sold+product.no_of_stock))*100)} label={`${Math.floor((product.no_of_stock/(product.sold+product.no_of_stock))*100)}%`}   variant="success" animated/>
+        </div>
+        <div >
+          {product.color?<div className='properties-container'><p>Color:</p>{product.color.map((actualcolor,index)=>{
+              return(
+                <div key={index}> 
+                <input
+                type="radio"
+                id={actualcolor}
+                value={actualcolor}
+                checked={selectedOption === actualcolor}
+            onChange={handleRadioChange}
+              />
+              <label htmlFor={actualcolor}>{actualcolor}</label></div>
+              )
+            })}</div>:''
+          }
+        </div>
+        <div>
+          {product.features?<div className='properties-container'><p>Features:</p>{product.features.map((feature,index)=>{
+              return(
+                <div key={index}>
+                  <input
+                type="radio"
+                id={feature}
+                value={feature}
+                checked={selectedOption === feature}
+            onChange={handleRadioChange}
+              />
+              <label htmlFor={feature}>{feature}</label>
+                </div>
+              )
+            })}</div>:''
+          }
+        </div>
+        <div>
+          {product.size?<div className='properties-container'><p>size:</p>{product.size.map((sizes,index)=>{
+              return(
+                <div key={index}>
+                      <input
+                type="radio"
+                id={sizes}
+                value={sizes}
+                checked={selectedOption === sizes}
+                onChange={handleRadioChange}
+              />
+              <label htmlFor={sizes}>{sizes}</label>
+                </div>
+              )
+            })}</div>:''
+          }
         </div>
         </div>
       </div>
