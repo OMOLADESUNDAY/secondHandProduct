c7c1d5020c7b188d8ab0767d630ab2addf4ab2af
spyer <sundayomoladee11@gmail.com>
2025-03-31 12:51:21 +0100

    register page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import '../styles/register.css'
 const Register = () => {
     const [eyeopen,setEyeOpen]=useState(false)
     const [inputType,setInputType]=useState('password')
@@ -14,23 +15,40 @@ const Register = () => {
         setInputType('password')
     }
   return (
-    <div>
-        <h4>Register</h4>
-        <section>
-            <form action="">
-                <div>
-                    <input type="text" placeholder='firstname' id='firstname'/>
-                        <label htmlFor="firstname">Firstname</label>
+    <div className='register-container'>
+        <h4 className='reg-txt'>Register</h4>
+        <section className='register-wrapper'>
+            <form action="" className='reg-form'>
+                <div className='two-input-container'>
+                    <div>
+                        <label htmlFor="firstname">Firstname</label><br />
+                        <input type="text" placeholder='firstname' id='firstname'/>
+                      
+                    </div>
+                   <div>
+                    <label htmlFor="lastname">Lastname</label><br />
                     <input type="text" placeholder='lastname' id='lastname'/>
-                        <label htmlFor="lastname">Lastname</label>
+                   </div>
                 </div>
-                    <input type="email" placeholder='email' id='email'/>
-                    <label htmlFor="email">email</label>
-                <div>
-                   <div><input type={inputType} placeholder='password' id='password'/>{eyeopen?<FaEye  onClick={()=>closeeyehandler()} />:<FaEyeSlash  onClick={()=>openeyehandler()}/>}</div> 
-                    <label htmlFor="password">password</label>   
-                    <div><input type={inputType} placeholder='confirm password' id='password'/>{eyeopen?<FaEye  onClick={()=>closeeyehandler()}/>:<FaEyeSlash  onClick={()=>openeyehandler()}/>}</div>
-                    <label htmlFor="confirmpassword">confirmpassword</label>
+
+
+                    <div className=''>
+                        <label htmlFor="email">email</label><br />
+                        <input type="email" placeholder='email' id='email'/>
+                    </div>
+                   
+                
+                
+                
+                <div className='two-input-container'>
+                    <div>
+                        <label htmlFor="password">password</label><br />
+                        <div><input type={inputType} placeholder='password' id='password'/>{eyeopen?<FaEye  onClick={()=>closeeyehandler()} />:<FaEyeSlash  onClick={()=>openeyehandler()}/>}</div> 
+                    </div>
+                    <div>
+                        <label htmlFor="confirmpassword">confirmpassword</label><br />
+                        <div><input type={inputType} placeholder='confirm password' id='password'/>{eyeopen?<FaEye  onClick={()=>closeeyehandler()}/>:<FaEyeSlash  onClick={()=>openeyehandler()}/>}</div>
+                    </div>
                 </div>
             <button type='submit'>submit</button>
             </form>
